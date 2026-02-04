95b0e281ef7f1718a0d68d65b220f1dae04c9a91
Fff-dot <daripandra@gmail.com>
2026-02-04 20:49:33 +0700

    Issue Error Deploy Railway

--- start.sh
+++ start.sh
@@ -1,2 +1,30 @@
-#!/bin/sh
-php artisan serve --host=0.0.0.0 --port=$((PORT))
+#!/bin/bash
+
+# Start PHP-FPM in background
+php-fpm -D
+
+# Wait for PHP-FPM to be ready
+sleep 2
+
+# Create nginx config with correct PORT
+cat > /etc/nginx/sites-available/default <<EOF
+server {
+    listen $PORT default_server;
+    root /app/public;
+    index index.php index.html;
+
+    location / {
+        try_files \$uri \$uri/ /index.php?\$query_string;
+    }
+
+    location ~ \.php$ {
+        fastcgi_pass 127.0.0.1:9000;
+        fastcgi_index index.php;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        include fastcgi_params;
+    }
+}
+EOF
+
+# Start nginx in foreground
+nginx -g 'daemon off;'
