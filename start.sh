a15ca8ac0ba4901c2a2534ab700d685e9fe6b831
Fff-dot <daripandra@gmail.com>
2026-02-04 19:19:31 +0700

    Issue Error Deploy Railway

--- start.sh
+++ start.sh
@@ -2,13 +2,10 @@
 # Start PHP-FPM in background
 php-fpm -D
 
-# Wait for PHP-FPM socket to be created
-while [ ! -S /run/php/php8.3-fpm.sock ]; do
-  echo "Waiting for PHP-FPM socket..."
-  sleep 1
-done
+# Wait a moment for PHP-FPM to start
+sleep 3
 
-echo "PHP-FPM socket ready, starting nginx..."
+echo "Starting nginx..."
 
 # Start nginx in foreground
 nginx -g 'daemon off;'
