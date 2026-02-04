82914391466a057ceddecfb8842d822f272b4497
Fff-dot <daripandra@gmail.com>
2026-02-04 20:43:13 +0700

    Issue Error Deploy Railway

--- start.sh
+++ start.sh
@@ -1,11 +1,2 @@
-#!/bin/bash
-# Start PHP-FPM in background
-php-fpm -D
-
-# Wait a moment for PHP-FPM to start
-sleep 3
-
-echo "Starting nginx..."
-
-# Start nginx in foreground
-nginx -g 'daemon off;'
+#!/bin/sh
+php artisan serve --host=0.0.0.0 --port=$((PORT))
